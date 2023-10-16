36d9613a2cb00d32f9213d4b7803cf2fccd7fadc
David Heidkamp <dheidkamp21@gmail.com>
2023-10-16 09:15:39 -0400

    add internship_project_2

--- features/steps/internship_registration_page_steps.py
+++ features/steps/internship_registration_page_steps.py
@@ -9,16 +9,27 @@ def open_registration_page(context):
     context.app.internship_registration_page.open_registration()
 
 
+
 @when('Enter some test information in the input fields')
 def enter_text(context):
     context.app.internship_registration_page.enter_information_name_field()
+    sleep(1)
     context.app.internship_registration_page.enter_information_company_field()
+    sleep(1)
     context.app.internship_registration_page.enter_information_phone_field()
+    sleep(1)
     context.app.internship_registration_page.enter_information_email_field()
+    sleep(1)
     context.app.internship_registration_page.enter_information_password_field()
+    sleep(1)
+    context.app.internship_registration_page.select_role()
+    sleep(1)
+    context.app.internship_registration_page.select_position()
+    sleep(1)
+    context.app.internship_registration_page.select_country()
+    sleep(1)
     context.app.internship_registration_page.enter_information_company_size()
-    sleep(5)
-
+    sleep(1)
 
 # def click_registration_boxes(context):
 #     context.app.internship_registration_page.select_from_dropdown("Developer")
@@ -27,3 +38,8 @@ def enter_text(context):
 @then('Verify the right information is present')
 def verify_registration(context):
     context.app.internship_registration_page.verify_registration_text_name_field()
+    context.app.internship_registration_page.verify_registration_text_company_field()
+    context.app.internship_registration_page.verify_registration_text_phone_field()
+    context.app.internship_registration_page.verify_registration_text_email_field()
+    context.app.internship_registration_page.verify_registration_text_password_field()
+    context.app.internship_registration_page.verify_registration_text_company_size()
--- pages/internship_registration_page.py
+++ pages/internship_registration_page.py
@@ -4,10 +4,15 @@ from selenium.webdriver.support.ui import Select
 
 class InternshipRegistrationPage(Page):
     NAME_FIELD = (By.ID, 'Full-Name')
+    Name_Input = 'FirstName'
     COMPANY_FIELD = (By.ID, 'Company-4')
+    COMPANY_Input = 'Company'
     PHONE_FIELD = (By.ID, 'phone2')
+    PHONE_INPUT = '1234567890'
     EMAIL_FIELD = (By.ID, 'Email-3')
+    EMAIL_INPUT = 'tester@testytest.com'
     PASSWORD_FIELD = (By.ID, 'field')
+    PASSWORD_INPUT = 'Password'
     ROLE_MENU_FIELD = (By.ID, 'Role')
     ROLE_DEVELOPER = (By.CSS_SELECTOR, "[value = 'Developer']")
     POSITION_MENU = (By.ID, 'Position')
@@ -15,6 +20,7 @@ class InternshipRegistrationPage(Page):
     COUNTRY_MENU = (By.ID, 'country-select')
     COUNTRY_USA = (By.CSS_SELECTOR, "[value='United States of America']")
     COMPANY_SIZE = (By.ID, 'Agents-amount')
+    COMPANY_SIZE_INPUT = '7654'
     def open_registration(self):
         self.driver.get('https://soft.reelly.io/sign-up')
 
@@ -22,7 +28,7 @@ class InternshipRegistrationPage(Page):
         self.input_text('FirstName', *self.NAME_FIELD)
 
     def enter_information_company_field (self):
-        self.input_text('CompanyName', *self.COMPANY_FIELD)
+        self.input_text('Company', *self.COMPANY_FIELD)
 
     def enter_information_phone_field(self):
         self.input_text('1234567890', *self.PHONE_FIELD)
@@ -31,32 +37,45 @@ class InternshipRegistrationPage(Page):
         self.input_text('tester@testytest.com', *self.EMAIL_FIELD)
 
     def enter_information_password_field(self):
-        self.input_text('password', *self.PASSWORD_FIELD)
+        self.input_text('Password', *self.PASSWORD_FIELD)
 
     def enter_information_company_size(self):
         self.input_text('7654', *self.COMPANY_SIZE)
-    #
 
-# def select_from_dropdown(self, option):
-#     select = Select(self.driver.find_element_by_id('*self.ROLE_MENU_FIELD'))
-#     select.select_by_visible_text(option)
+    def select_from_dropdown(self, dropdown_locator, option):
+        dropdown = self.find_element(*dropdown_locator)
+        select = Select(dropdown)
+        select.select_by_visible_text(option)
 
-    # def select_department(self, alias: str):
-    #     drop_down = self.find_element(*self.ROLE_MENU_FIELD)
-    #     select = Select(drop_down)
-    #     select.select_by_visible_text(alias)
-    #
-    # def click_boxes(self):
-    #     self.click(*self.ROLE_MENU_FIELD)
-    #     self.click(*self.ROLE_DEVELOPER)
-    #     self.click(*self.POSITION_MENU)
-    #     self.click(*self.POSITION_SELLER_MANAGER)
-    #     self.click(*self.COUNTRY_MENU)
-    #     self.click(*self.COUNTRY_USA)
+    def select_role(self):
+        self.select_from_dropdown(self.ROLE_MENU_FIELD, "Developer")
+
+    def select_position(self):
+        self.select_from_dropdown(self.POSITION_MENU, "Director / CEO")
+
+    def select_country(self):
+        self.select_from_dropdown(self.COUNTRY_MENU, "United States of America")
 
     def verify_registration_text_name_field(self):
-        expected_text = 'FirstName'
-        actual_text = self.find_element(*self.NAME_FIELD).text
-        assert actual_text == expected_text, f'Expected {expected_text}, but got {actual_text}'
+        actual_name = self.find_element(*self.NAME_FIELD).get_attribute('value')
+        assert actual_name == self.Name_Input, f'Expected name: {self.Name_Input}, Actual name: {actual_name}'
 
+    def verify_registration_text_company_field(self):
+        actual_name = self.find_element(*self.COMPANY_FIELD).get_attribute('value')
+        assert actual_name == self.COMPANY_Input, f'Expected name: {self.COMPANY_Input}, Actual name: {actual_name}'
 
+    def verify_registration_text_phone_field(self):
+        actual_name = self.find_element(*self.PHONE_FIELD).get_attribute('value')
+        assert actual_name == self.PHONE_INPUT, f'Expected name: {self.PHONE_INPUT}, Actual name: {actual_name}'
+
+    def verify_registration_text_email_field(self):
+        actual_name = self.find_element(*self.EMAIL_FIELD).get_attribute('value')
+        assert actual_name == self.EMAIL_INPUT, f'Expected name: {self.EMAIL_INPUT}, Actual name: {actual_name}'
+
+    def verify_registration_text_password_field(self):
+        actual_name = self.find_element(*self.PASSWORD_FIELD).get_attribute('value')
+        assert actual_name == self.PASSWORD_INPUT, f'Expected name: {self.PASSWORD_INPUT}, Actual name: {actual_name}'
+
+    def verify_registration_text_company_size(self):
+        actual_name = self.find_element(*self.COMPANY_SIZE).get_attribute('value')
+        assert actual_name == self.COMPANY_SIZE_INPUT, f'Expected name: {self.COMPANY_SIZE_INPUT}, Actual name: {actual_name}'
