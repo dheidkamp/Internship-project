7205d97ebd2652a2341deb665154584f3e537828
David Heidkamp <dheidkamp21@gmail.com>
2023-10-17 21:44:11 -0400

    updated application.py

--- app/application.py
+++ app/application.py
@@ -1,15 +1,14 @@
 from pages.main_page import MainPage
 from pages.privacy_notice_page import PrivacyNotice
 from pages.header import Header
-from pages.search_result_page import SearchResultPage
+# from pages.search_result_page import SearchResultPage
 from pages.signin_page import SigninPage
-from pages.amazon_terms_and_conditions_page import TermsConditionPage
-
+from pages.internship_registration_page import InternshipRegistrationPage
 class Application:
     def __init__(self, driver):
         self.main_page = MainPage(driver)
         self.header = Header(driver)
-        self.search_result_page = SearchResultPage(driver)
+        # self.search_result_page = SearchResultPage(driver)
         self.signin_page = SigninPage(driver)
-        self.amazon_terms_conditions_page = TermsConditionPage(driver)
         self.privacy_notice_page = PrivacyNotice(driver)
+        self.internship_registration_page = InternshipRegistrationPage(driver)
