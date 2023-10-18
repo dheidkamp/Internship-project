1f53717f2915677603a668b28a2a9d8f1cf8d79c
David Heidkamp <dheidkamp21@gmail.com>
2023-10-18 12:52:22 -0400

    updated environment.py

--- features/environment.py
+++ features/environment.py
@@ -4,19 +4,46 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.wait import WebDriverWait
 from app.application import Application
 
+
 def browser_init(context):
     """
     :param context: Behave context
     """
-    service = Service(executable_path=r'C:\Users\dheid\python-selenium-automation\chromedriver.exe')
-    # service = Service(ChromeDriverManager().install())
-    context.driver = webdriver.Chrome(service=service)
+    # CHROME BROWSER
+    # service = Service(executable_path=r'C:\Users\dheid\python-selenium-automation\chromedriver.exe')
+    # # service = Service(ChromeDriverManager().install())
+    # context.driver = webdriver.Chrome(service=service)
+
+    # # FIREFOX BROWSER
+    # service = Service(executable_path=r'\Users\dheid\OneDrive\Desktop\Automation_Internship\geckodriver')
+    # context.driver = webdriver.Firefox(service=service)
+
+    # # HEADLESS MODE CHROME
+    # options = webdriver.ChromeOptions()
+    # options.add_argument('--headless')
+    # service = Service(executable_path=r'C:\Users\dheid\python-selenium-automation\chromedriver.exe')
+    # context.driver = webdriver.Chrome(
+    #     options=options,
+    #     service=service
+    # )
+
+    # # HEADLESS MODE FIREFOX
+    options = webdriver.FirefoxOptions()
+    options.add_argument('--headless')
+    service = Service(executable_path=r'\Users\dheid\OneDrive\Desktop\Automation_Internship\geckodriver')
+    context.driver = webdriver.Firefox(
+        options=options,
+        service=service
+    )
+
+
 
     context.driver.maximize_window()
     context.driver.implicitly_wait(4)
     context.driver.wait = WebDriverWait(context.driver, 10)
 
     context.app = Application(context.driver)
+
 
 def before_scenario(context, scenario):
     print('\nStarted scenario: ', scenario.name)
