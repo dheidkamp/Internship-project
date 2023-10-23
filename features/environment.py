307b2123f1a7e7dbd191fdd4b5ab1d19a8da96eb
David Heidkamp <dheidkamp21@gmail.com>
2023-10-23 16:38:35 -0400

    updated environment.py

--- features/environment.py
+++ features/environment.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.chrome.options import Options
 from app.application import Application
 
 
@@ -27,15 +28,28 @@ def browser_init(context):
     #     service=service
     # )
 
-    # # HEADLESS MODE FIREFOX
-    options = webdriver.FirefoxOptions()
-    options.add_argument('--headless')
-    service = Service(executable_path=r'\Users\dheid\OneDrive\Desktop\Automation_Internship\geckodriver')
-    context.driver = webdriver.Firefox(
-        options=options,
-        service=service
-    )
+    # # # HEADLESS MODE FIREFOX
+    # options = webdriver.FirefoxOptions()
+    # options.add_argument('--headless')
+    # service = Service(executable_path=r'\Users\dheid\OneDrive\Desktop\Automation_Internship\geckodriver')
+    # context.driver = webdriver.Firefox(
+    #     options=options,
+    #     service=service
+    # )
+    # Register for BrowserStack, then grab it from https://www.browserstack.com/accounts/settings
+    bs_user = 'davidheidkamp_niuIE7'
+    bs_key = 'iAiL5eUMUEz8rsu2osoQ'
+    url = f'http://{bs_user}:{bs_key}@hub-cloud.browserstack.com/wd/hub'
 
+    options = Options()
+    bstack_options = {
+        'os': 'OS X',
+        'osVersion': 'Big Sur',
+        'browserName': 'Chrome',
+        'sessionName': 'Registration_Page'
+    }
+    options.set_capability('bstack:options', bstack_options)
+    context.driver = webdriver.Remote(command_executor=url, options=options)
 
 
     context.driver.maximize_window()
